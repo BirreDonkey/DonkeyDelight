23b718905c4c789ccb9377ac2440b938e701af41
Footsorepuppy42 <robin.cuyckens@gmail.com>
2023-06-11 11:10:06 +0200

    Pan Model Added

--- src/main/java/net/mcreator/donkeydelight/block/PanBlock.java
+++ src/main/java/net/mcreator/donkeydelight/block/PanBlock.java
@@ -1,6 +1,9 @@
 
 package net.mcreator.donkeydelight.block;
 
+import net.minecraft.world.phys.shapes.VoxelShape;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.block.state.BlockState;
@@ -16,12 +19,22 @@ import java.util.Collections;
 
 public class PanBlock extends Block {
 	public PanBlock() {
-		super(BlockBehaviour.Properties.of(Material.STONE).sound(SoundType.GRAVEL).strength(1f, 10f));
+		super(BlockBehaviour.Properties.of(Material.STONE).sound(SoundType.GRAVEL).strength(1f, 10f).noOcclusion().isRedstoneConductor((bs, br, bp) -> false));
+	}
+
+	@Override
+	public boolean propagatesSkylightDown(BlockState state, BlockGetter reader, BlockPos pos) {
+		return true;
 	}
 
 	@Override
 	public int getLightBlock(BlockState state, BlockGetter worldIn, BlockPos pos) {
-		return 15;
+		return 0;
+	}
+
+	@Override
+	public VoxelShape getVisualShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
+		return Shapes.empty();
 	}
 
 	@Override
